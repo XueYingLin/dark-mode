f5c6faca7e7e53be4967e9eb48fad561d5e3f64c
Nikki Lin <nikkiylin@gmail.com>
2020-03-11 15:46:31 -0700

    have the Narbat.jsx done.

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -5,11 +5,11 @@ export const useDarkMode = (key, initialValue) => {
     const [value, setValue] = useLocalStorage(key, initialValue);
 
     useEffect(() => {
-        if (useLocalStorage(key) === true)  {
+        if (value)  {
             document.body.classList.add('dark-mode');
         } else {
             document.body.classList.remove('dark-mode');
         }
-    });
+    }, [value]);
     return [value, setValue];
 }
