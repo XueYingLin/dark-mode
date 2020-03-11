8cd5db89232fea35a4f194cde520e99197071d63
Nikki Lin <nikkiylin@gmail.com>
2020-03-11 15:20:26 -0700

    useDarkMode with useEffect inside

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,5 +1,15 @@
 import { useLocalStorage } from "./useLocalStorage";
+import { useEffect } from "react";
 
-export consts useDarkMode = (key, initialValue) => {
+export const useDarkMode = (key, initialValue) => {
     const [value, setValue] = useLocalStorage(key, initialValue);
+
+    useEffect(() => {
+        if (useLocalStorage(key) === true)  {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+    });
+    return [value, setValue];
 }
